ad0ce55a471656f6a4407f0e05078be24e57a3c0
lanxiaozhi <kouz15@mails.tsinghua.edu.cn>
2018-01-02 14:09:39 +0800

    modify

--- Parse/frontend.c
+++ Parse/frontend.c
@@ -1,5 +1,14 @@
 int main(){
-    int i = 1;
-    printf("123");
-    return 0;
+while (i != 0 ) {
+    i = i - 1;
+}
+if ( i == 1 ){
+	i = i + 1;
+}
+else if ( i == 2 ) {
+    i = i + 2;
+}
+else {
+    i = i + 3;
+}
 }
