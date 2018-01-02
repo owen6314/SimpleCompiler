848ced3635eb8351da35693ff876e0ad2cf94f34
朱泓宇 <hy-zhu15@mails.tsinghua.edu.cn>
2018-01-02 13:41:03 +0800

    emit

--- Parse/translate_visitor.py
+++ Parse/translate_visitor.py
@@ -2,9 +2,9 @@
 from antlr4 import *
 
 if __name__ is not None and "." in __name__:
-  from .SimpleCParser import SimpleCParser
+    from .SimpleCParser import SimpleCParser
 else:
-  from SimpleCParser import SimpleCParser
+    from SimpleCParser import SimpleCParser
 
 # This class defines a complete generic visitor for a parse tree produced by SimpleCParser.
 
@@ -12,143 +12,145 @@ from SimpleCVisitor import SimpleCVisitor
 
 
 class TranslateVisitor(SimpleCVisitor):
-  indentation = 0
-  
-  def print_indentation(self):
-    for i in range(self.indentation):
-      print("    ", end="")
-      
-  def emit(self, *args):
-    for arg in args:
-      print(arg, end="")
-  
-  # Visit a parse tree produced by SimpleCParser#start.
-  def visitStart(self, ctx: SimpleCParser.StartContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#include.
-  def visitInclude(self, ctx: SimpleCParser.IncludeContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#function.
-  def visitFunction(self, ctx: SimpleCParser.FunctionContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#mType.
-  def visitMType(self, ctx: SimpleCParser.MTypeContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#content.
-  def visitContent(self, ctx: SimpleCParser.ContentContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#block.
-  def visitBlock(self, ctx: SimpleCParser.BlockContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#ifBlock.
-  def visitIfBlock(self, ctx: SimpleCParser.IfBlockContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#elifBlock.
-  def visitElifBlock(self, ctx: SimpleCParser.ElifBlockContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#elseBlock.
-  def visitElseBlock(self, ctx: SimpleCParser.ElseBlockContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#whileBlock.
-  def visitWhileBlock(self, ctx: SimpleCParser.WhileBlockContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#condition.
-  def visitCondition(self, ctx: SimpleCParser.ConditionContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#stat.
-  def visitStat(self, ctx: SimpleCParser.StatContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#expr.
-  def visitExpr(self, ctx: SimpleCParser.ExprContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#designator.
-  def visitDesignator(self, ctx: SimpleCParser.DesignatorContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#arrayNoInit.
-  def visitArrayNoInit(self, ctx: SimpleCParser.ArrayNoInitContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#exprFunc.
-  def visitExprFunc(self, ctx: SimpleCParser.ExprFuncContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#strlenFunc.
-  def visitStrlenFunc(self, ctx: SimpleCParser.StrlenFuncContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#atoiFunc.
-  def visitAtoiFunc(self, ctx: SimpleCParser.AtoiFuncContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#isdigitFunc.
-  def visitIsdigitFunc(self, ctx: SimpleCParser.IsdigitFuncContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#declareStat.
-  def visitDeclareStat(self, ctx: SimpleCParser.DeclareStatContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#assignStat.
-  def visitAssignStat(self, ctx: SimpleCParser.AssignStatContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#returnStat.
-  def visitReturnStat(self, ctx: SimpleCParser.ReturnStatContext):
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#printfStat.
-  def visitPrintfStat(self, ctx: SimpleCParser.PrintfStatContext):
-    if ctx.getChildCount() == 5:
-      self.emit("print(")
-      self.visit(ctx.getChild(2))
-      self.emit(")")
-    else:
-      self.emit("print(")
-    return
-  
-  # Visit a parse tree produced by SimpleCParser#mOperator.
-  def visitMOperator(self, ctx: SimpleCParser.MOperatorContext):
-    self.emit(ctx.OPERATOR().getText())
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#mConnector.
-  def visitMConnector(self, ctx: SimpleCParser.MConnectorContext):
-    self.emit(ctx.CONNECTOR().getText())
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#mInt.
-  def visitMInt(self, ctx: SimpleCParser.MIntContext):
-    self.emit(ctx.INT().getText())
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#mChar.
-  def visitMChar(self, ctx: SimpleCParser.MCharContext):
-    self.emit(ctx.CHAR().getText())
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#mString.
-  def visitMString(self, ctx: SimpleCParser.MStringContext):
-    self.emit(ctx.STRING().getText())
-    return self.visitChildren(ctx)
-  
-  # Visit a parse tree produced by SimpleCParser#mID.
-  def visitMID(self, ctx: SimpleCParser.MIDContext):
-    self.emit(ctx.ID().getText())
-    return self.visitChildren(ctx)
+    indentation = 0
+    
+    def print_indentation(self):
+        for i in range(self.indentation):
+            print("    ", end="")
+    
+    def emit(self, *args):
+        for arg in args:
+            print(arg, end="")
+    
+    # Visit a parse tree produced by SimpleCParser#start.
+    def visitStart(self, ctx: SimpleCParser.StartContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#include.
+    def visitInclude(self, ctx: SimpleCParser.IncludeContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#function.
+    def visitFunction(self, ctx: SimpleCParser.FunctionContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#mType.
+    def visitMType(self, ctx: SimpleCParser.MTypeContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#content.
+    def visitContent(self, ctx: SimpleCParser.ContentContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#block.
+    def visitBlock(self, ctx: SimpleCParser.BlockContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#ifBlock.
+    def visitIfBlock(self, ctx: SimpleCParser.IfBlockContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#elifBlock.
+    def visitElifBlock(self, ctx: SimpleCParser.ElifBlockContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#elseBlock.
+    def visitElseBlock(self, ctx: SimpleCParser.ElseBlockContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#whileBlock.
+    def visitWhileBlock(self, ctx: SimpleCParser.WhileBlockContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#condition.
+    def visitCondition(self, ctx: SimpleCParser.ConditionContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#stat.
+    def visitStat(self, ctx: SimpleCParser.StatContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#expr.
+    def visitExpr(self, ctx: SimpleCParser.ExprContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#designator.
+    def visitDesignator(self, ctx: SimpleCParser.DesignatorContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#arrayNoInit.
+    def visitArrayNoInit(self, ctx: SimpleCParser.ArrayNoInitContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#exprFunc.
+    def visitExprFunc(self, ctx: SimpleCParser.ExprFuncContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#strlenFunc.
+    def visitStrlenFunc(self, ctx: SimpleCParser.StrlenFuncContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#atoiFunc.
+    def visitAtoiFunc(self, ctx: SimpleCParser.AtoiFuncContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#isdigitFunc.
+    def visitIsdigitFunc(self, ctx: SimpleCParser.IsdigitFuncContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#declareStat.
+    def visitDeclareStat(self, ctx: SimpleCParser.DeclareStatContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#assignStat.
+    def visitAssignStat(self, ctx: SimpleCParser.AssignStatContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#returnStat.
+    def visitReturnStat(self, ctx: SimpleCParser.ReturnStatContext):
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#printfStat.
+    def visitPrintfStat(self, ctx: SimpleCParser.PrintfStatContext):
+        if ctx.getChildCount() == 5:
+            self.emit("print(")
+            self.visit(ctx.getChild(2))
+            self.emit(")")
+        else:
+            self.emit("print(")
+            self.visit(ctx.getChild(2))
+            self.emit("%")
+        return
+    
+    # Visit a parse tree produced by SimpleCParser#mOperator.
+    def visitMOperator(self, ctx: SimpleCParser.MOperatorContext):
+        self.emit(ctx.OPERATOR().getText())
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#mConnector.
+    def visitMConnector(self, ctx: SimpleCParser.MConnectorContext):
+        self.emit(ctx.CONNECTOR().getText())
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#mInt.
+    def visitMInt(self, ctx: SimpleCParser.MIntContext):
+        self.emit(ctx.INT().getText())
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#mChar.
+    def visitMChar(self, ctx: SimpleCParser.MCharContext):
+        self.emit(ctx.CHAR().getText())
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#mString.
+    def visitMString(self, ctx: SimpleCParser.MStringContext):
+        self.emit(ctx.STRING().getText())
+        return self.visitChildren(ctx)
+    
+    # Visit a parse tree produced by SimpleCParser#mID.
+    def visitMID(self, ctx: SimpleCParser.MIDContext):
+        self.emit(ctx.ID().getText())
+        return self.visitChildren(ctx)
 
 
 del SimpleCParser
