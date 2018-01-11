d1dbcdccacc2cff40202932d9ada9e93dbe44159
朱泓宇 <hy-zhu15@mails.tsinghua.edu.cn>
2018-01-11 22:00:01 +0800

    the end

--- Parse/parse.py
+++ Parse/parse.py
@@ -11,7 +11,7 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = SimpleCParser(stream)
     tree = parser.start()
-    output_file = open("cal.py", "w+")
+    output_file = open(argv[2], "w+")
     eval = TranslateVisitor(output_file)
     eval.visit(tree)
     output_file.close()
